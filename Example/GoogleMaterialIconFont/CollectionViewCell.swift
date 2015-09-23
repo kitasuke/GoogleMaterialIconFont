2653df4edb2debbadbee7289dc5c95d63e810ff2
kitasuke <yusuke2759@gmail.com>
2015-09-23 16:20:22 +0900

    Initial commit

---
+++ Example/GoogleMaterialIconFont/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  GoogleMaterialIconFont
+//
+//  Created by Yusuke Kita on 9/23/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import GoogleMaterialIconFont
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var label: UILabel!
+    
+    func setup(font: MaterialIconFont) {
+        label.text = String.materialIcon(font)
+        label.font = UIFont.materialIconOfSize(32)
+        label.textColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
+    }
+}
---
+++ Pod/Classes/GoogleMaterialIconFont.swift
@@ -0,0 +1,61 @@
+//
+//  GoogleMaterialIconFont.swift
+//  Pods
+//
+//  Created by Yusuke Kita on 9/23/15.
+//
+//
+
+import UIKit
+
+public extension String {
+    public static func materialIcon(font: MaterialIconFont) -> String {
+        return IconFont.codes[font.rawValue]
+    }
+}
+
+public extension NSString {
+    public static func meterialIcon(font: MaterialIconFont) -> NSString {
+        return NSString(string: String.materialIcon(font))
+    }
+}
+
+public extension UIFont {
+    public static func materialIconOfSize(size: CGFloat) -> UIFont {
+        var onceToken: dispatch_once_t = 0
+        let name = "GoogleMaterialIconFont"
+        
+        if UIFont.fontNamesForFamilyName(name).count == 0 {
+            dispatch_once(&onceToken, { () -> Void in
+                FontLoader.loadFont(name)
+            })
+        }
+        return UIFont(name: name, size: size)!
+    }
+}
+
+private class FontLoader {
+    class func loadFont(name: String) {
+        let bundle = NSBundle(forClass: FontLoader.self)
+        let identifier = bundle.bundleIdentifier
+        let fileExtension = "ttf"
+        
+        let fontURL: NSURL
+        if identifier?.hasPrefix("org.cocoapods") == true {
+            fontURL = bundle.URLForResource(name, withExtension: fileExtension, subdirectory: "GoogleMaterialIconFont.bundle")!
+        } else {
+            fontURL = bundle.URLForResource(name, withExtension: fileExtension)!
+        }
+        
+        let data = NSData(contentsOfURL: fontURL)
+        let provider = CGDataProviderCreateWithCFData(data)
+        let font = CGFontCreateWithDataProvider(provider)!
+        
+        var error: Unmanaged<CFError>?
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
+            let nsError = error!.takeUnretainedValue() as NSError
+            NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+        }
+    }
+}
