50cc799411c1e7eb2d01d9d03986d026edde808d
kitasuke <yusuke2759@gmail.com>
2016-01-27 20:25:31 -0800

    Update example project

---
+++ Example/Pods/Target Support Files/GoogleMaterialIconFont/GoogleMaterialIconFont-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GoogleMaterialIconFont : NSObject
+@end
+@implementation PodsDummy_GoogleMaterialIconFont
+@end
---
+++ Example/Pods/Target Support Files/GoogleMaterialIconFont/GoogleMaterialIconFont-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "GoogleMaterialIconFont.h"
+
+FOUNDATION_EXPORT double GoogleMaterialIconFontVersionNumber;
+FOUNDATION_EXPORT const unsigned char GoogleMaterialIconFontVersionString[];
+
