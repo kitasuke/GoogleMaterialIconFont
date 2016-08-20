a2f3bb2f6c74b8bb0d37d46086bc3c58b31dd23e
kitasuke <yusuke2759@gmail.com>
2016-08-21 07:23:56 +0900

    Convert to Swift 3.0

--- Example/GoogleMaterialIconFont/CollectionViewCell.swift
+++ Example/GoogleMaterialIconFont/CollectionViewCell.swift
@@ -12,7 +12,7 @@ import GoogleMaterialIconFont
 class CollectionViewCell: UICollectionViewCell {
     @IBOutlet var label: UILabel!
     
-    func setup(font: MaterialIconFont) {
+    func setup(_ font: MaterialIconFont) {
         label.text = String.materialIcon(font)
         label.font = UIFont.materialIconOfSize(32)
         label.textColor = UIColor(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
--- Pod/Classes/GoogleMaterialIconFont.swift
+++ Pod/Classes/GoogleMaterialIconFont.swift
@@ -9,24 +9,24 @@
 import UIKit
 
 public extension String {
-    public static func materialIcon(font: MaterialIconFont) -> String {
+    public static func materialIcon(_ font: MaterialIconFont) -> String {
         return IconFont.codes[font.rawValue]
     }
 }
 
 public extension NSString {
-    public static func materialIcon(font: MaterialIconFont) -> NSString {
+    public static func materialIcon(_ font: MaterialIconFont) -> NSString {
         return NSString(string: String.materialIcon(font))
     }
 }
 
 public extension UIFont {
-    public static func materialIconOfSize(size: CGFloat) -> UIFont {
-        var onceToken: dispatch_once_t = 0
+    public static func materialIconOfSize(_ size: CGFloat) -> UIFont {
+        var onceToken: Int = 0
         let filename = "MaterialIcons-Regular"
         let fontname = "Material Icons"
 
-        if UIFont.fontNamesForFamilyName(fontname).count == 0 {
+        if UIFont.fontNames(forFamilyName: fontname).count == 0 {
             dispatch_once(&onceToken, { () -> Void in
                 FontLoader.loadFont(filename)
             })
@@ -36,27 +36,27 @@ public extension UIFont {
 }
 
 private class FontLoader {
-    class func loadFont(name: String) {
-        let bundle = NSBundle(forClass: FontLoader.self)
+    class func loadFont(_ name: String) {
+        let bundle = Bundle(for: FontLoader.self)
         let identifier = bundle.bundleIdentifier
         let fileExtension = "ttf"
         
-        let fontURL: NSURL
+        let fontURL: URL
         if identifier?.hasPrefix("org.cocoapods") == true {
-            fontURL = bundle.URLForResource(name, withExtension: fileExtension, subdirectory: "GoogleMaterialIconFont.bundle")!
+            fontURL = bundle.url(forResource: name, withExtension: fileExtension, subdirectory: "GoogleMaterialIconFont.bundle")!
         } else {
-            fontURL = bundle.URLForResource(name, withExtension: fileExtension)!
+            fontURL = bundle.url(forResource: name, withExtension: fileExtension)!
         }
         
-        let data = NSData(contentsOfURL: fontURL)
-        let provider = CGDataProviderCreateWithCFData(data)
-        let font = CGFontCreateWithDataProvider(provider)!
+        let data = try? Data(contentsOf: fontURL)
+        let provider = CGDataProvider(data: data as! CFData)
+        let font = CGFont(provider!)
         
         var error: Unmanaged<CFError>?
         if !CTFontManagerRegisterGraphicsFont(font, &error) {
-            let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
+            let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
             let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
-            NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
+            NSException(name: NSExceptionName.internalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
         }
     }
 }
